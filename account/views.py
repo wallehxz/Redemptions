cea9d74c615034aa57a870d6f631be578f9fc0a9
huaxuan <wallehxz@gmail.com>
2025-06-02 19:48:37 +0800

    登录跳转页参数默认

--- account/views.py
+++ account/views.py
@@ -19,17 +19,14 @@ def login_user(request):
     if request.method == 'POST':
         mobile = request.POST.get('mobile')
         captcha = request.POST.get('captcha')
-        redirect_path = request.POST.get('redirect_path')
+        redirect_path = request.POST.get('redirect_path', 'home')
         mobile = re.sub(r'\D', '', mobile)
         user = Consumer.objects.filter(mobile=mobile).first()
         if user and user.check_captcha(captcha):
             login(request, user)
             user.login_at = datetime.now()
             user.save()
-            if redirect_path:
-                return redirect(redirect_path)
-            return redirect('home')
-    return redirect('home')
+        return redirect(redirect_path)
 
 
 def logout_user(request):
