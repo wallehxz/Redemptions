79cc79b0ec3ee072870394917c95846fe1473c23
huaxuan <wallehxz@gmail.com>
2025-06-06 15:16:46 +0800

    中文输入法时调整

--- assets/javascript/home_index.js
+++ assets/javascript/home_index.js
@@ -10,15 +10,16 @@ inputs.forEach((input, index) => {
 
     input.addEventListener('compositionend', (e) => {
         isComposing = false;
+        e.target.value = e.target.value.toUpperCase();
         e.target.value = e.target.value.replace(/[^a-zA-Z0-9]/g, '');
     });
 
     input.addEventListener('input', (e) => {
         // 限制输入长度为 4
-        if (!isComposing) {
-            e.target.value = e.target.value.toUpperCase();
-            e.target.value = e.target.value.replace(/[^a-zA-Z0-9]/g, '');
-        }
+        // if (!isComposing) {
+        //     e.target.value = e.target.value.toUpperCase();
+        //     e.target.value = e.target.value.replace(/[^a-zA-Z0-9]/g, '');
+        // }
 
         if (e.target.value.length > 4 && index > 0) {
             e.target.value = e.target.value.slice(0, 4);
